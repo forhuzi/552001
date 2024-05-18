afd14219ddef4815d3953334cf56096e6415c3dd
forhuzi <144129125+forhuzi@users.noreply.github.com>
2024-05-18 13:45:28 +0700

    Add files via upload

---
+++ goodnight/assets/script/script.js
@@ -0,0 +1,51 @@
+var audio = document.getElementById('background-audio');
+    audio.addEventListener('ended', function() {
+        audio.currentTime = 0;
+        audio.play();
+});
+
+var loaded = false;
+
+document.querySelector("#btn").onclick = function (){
+    if (audio.paused) {
+        audio.play();
+    }
+    $('#btn').hide();
+    $('.loadingLove').fadeIn('slow', function() {
+        setTimeout(function() {
+            $('.loadingLove').fadeOut('fast', function() {
+                loaded = true;
+                updateBackground(media);
+                $('.christmas').fadeIn('slow');
+            });
+        }, 5000);
+    });
+}
+
+function updateBackground(media) {
+    if (loaded) {
+        if (media.matches) {
+            $('body').css(
+                {
+                    "background-image": "url(../assets/img/nightSky.jpg)",
+                    "background-repeat": "none",
+                    "background-size": "100% 220px"
+                }
+            )
+        } else {
+            $('body').css(
+                {
+                    "background-image": "url(../assets/img/nightSky.jpg)",
+                    "background-repeat": "none",
+                    "background-size": "100% 100%"
+                }
+            )
+        }
+    }
+}
+  
+var media = window.matchMedia("(max-width: 566px)");
+
+media.addEventListener("change", function() {
+    updateBackground(media);
+});
